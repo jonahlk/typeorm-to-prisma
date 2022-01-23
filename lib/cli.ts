0c296ff39a8e87a417185fb4d1e75c4a6458112c
Jonah <jonah.lakemeier@kreatize.com>
2022-01-23 22:19:10 +0100

    [lib/cli.ts] Updated env vars

--- lib/cli.ts
+++ lib/cli.ts
@@ -1,24 +1,25 @@
 #! /usr/bin/env node
-const { program } = require('commander');
-const {generatePrismaSchemaFromEntities} = require('./index')
+const {program} = require('commander');
+const {generatePrismaSchemaFromEntities} = require('./index');
+require('dotenv').config({path: `.env`});
 
-program.requiredOption('-e, --entities <path>', 'Path to entities')
-program.option('-s, --schema-path <path>', 'Path for the schema file to be created')
+program.requiredOption('-e, --entities <path>', 'Path to entities');
+program.option('-s, --schema-path <path>', 'Path for the schema file to be created');
 program.showHelpAfterError();
 
-const {POSTGRES_HOST, POSTGRES_USERNAME, POSTGRES_PASSWORD, POSTGRES_DB} = process.env
-
-if(!POSTGRES_HOST) {
-  program.requiredOption('-h, --db-host <key>', 'Database host ')
+const {POSTGRESQL_HOST, POSTGRESQL_USERNAME, POSTGRESQL_PASSWORD, POSTGRESQL_DB} = process.env;
+console.log(process.env);
+if (!POSTGRESQL_HOST) {
+  program.requiredOption('-h, --db-host <key>', 'Database host ');
 }
-if(!POSTGRES_USERNAME) {
-  program.requiredOption('-u, --db-username <key>', 'Database username')
+if (!POSTGRESQL_USERNAME) {
+  program.requiredOption('-u, --db-username <key>', 'Database username');
 }
-if(!POSTGRES_PASSWORD) {
-  program.requiredOption('-p, --db-password <key>', 'Database password')
+if (!POSTGRESQL_PASSWORD) {
+  program.requiredOption('-p, --db-password <key>', 'Database password');
 }
-if(!POSTGRES_DB) {
-  program.requiredOption('-d, --db-database <key>', 'Database name')
+if (!POSTGRESQL_DB) {
+  program.requiredOption('-d, --db-database <key>', 'Database name');
 
 }
 
@@ -28,11 +29,11 @@ const {entities, dbHost, dbUsername, dbPassword, dbDatabase, schemaPath} = optio
 
 generatePrismaSchemaFromEntities(
   entities,
-  dbHost || POSTGRES_HOST,
-  dbUsername || POSTGRES_USERNAME,
-  dbPassword || POSTGRES_PASSWORD,
-  dbDatabase || POSTGRES_DB,
-  schemaPath
+  dbHost || POSTGRESQL_HOST,
+  dbUsername || POSTGRESQL_USERNAME,
+  dbPassword || POSTGRESQL_PASSWORD,
+  dbDatabase || POSTGRESQL_DB,
+  schemaPath,
 )
 .then(() => {
   console.log("The schema.prisma file has been generated successfully")
